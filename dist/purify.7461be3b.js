79beda44bc6c6ec4ce230cc05c4484f406a08d79
Szelepcsényi Zsolt <szelepcsenyi.zsolt@evista.hu>
2022-02-18 14:08:37 +0100

    Fix

--- dist/purify.7461be3b.js
+++ dist/purify.7461be3b.js
@@ -1599,7 +1599,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59587" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56067" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
